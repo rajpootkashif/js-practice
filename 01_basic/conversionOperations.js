a1e73a92927bd1185328d45bc13b9a77cc076eb7
Raja Kashif <146663811+rajpootkashif@users.noreply.github.com>
2024-03-19 23:18:15 +0000

    conversion of datatypes

---
+++ 01_basic/conversionOperations.js
@@ -0,0 +1,38 @@
+let score = true
+// console.log(typeof (score));
+let value = Number(score)
+// console.log(typeof value);
+// console.log(value);
+
+
+
+let myName = "kashif"
+console.log(myName);
+// console.log(typeof number);
+// let newnumber = Boolean(number) 
+// console.log(typeof newnumber);
+
+// console.log(typeof myName);
+let myNewName = Number (myName)
+console.log(myNewName);
+console.log(typeof myNewName);
+
+// "33" => 33
+// "33abc" or "kashif" => NaN
+// true => 1 / false => 0
+
+
+
+
+// let scoree = undefined
+// let age = "65"
+// console.log(typeof age);
+
+// let valueInNumber = Number(score)
+// let value = Number(age) 
+// console.log(value);
+// console.log(typeof value);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
