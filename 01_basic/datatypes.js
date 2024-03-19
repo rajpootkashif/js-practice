d1c490a6dd245b6c1833d13437e04a9cb31a9796
Raja Kashif <146663811+rajpootkashif@users.noreply.github.com>
2024-03-19 22:30:15 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as nerwer version
+
+// alert(3 + 3) we are using this in NodeJs, not in browser
+
+// like console.log(3 +
+// 3) this is not a good practice
+
+// code readability should be high that can be under stand by anyone 
+
+let name = "kashif"
+let age = 24
+let isLoggedIn = true
+let state = null;
+
+
+// number
+// bigint
+// string => ""
+// boolearn => yes or no
+// null => standalone Value discuss about this more later
+// undefined
+// symbol => this one is use for unique
+
+//object
+
+console.log(typeof null);
+// null is a object
