3b710d27ade50bbfb0fe0f67e7a6d8c9c5d03a46
Raja Kashif <146663811+rajpootkashif@users.noreply.github.com>
2024-03-20 08:39:14 +0000

    operations_01

--- 01_basic/conversionOperations.js
+++ 01_basic/conversionOperations.js
@@ -1,21 +1,21 @@
-let score = true
-// console.log(typeof (score));
-let value = Number(score)
-// console.log(typeof value);
-// console.log(value);
+// let score = true
+// // console.log(typeof (score));
+// let value = Number(score)
+// // console.log(typeof value);
+// // console.log(value);
 
 
 
-let myName = "kashif"
-console.log(myName);
-// console.log(typeof number);
-// let newnumber = Boolean(number) 
-// console.log(typeof newnumber);
+// let myName = "kashif"
+// console.log(myName);
+// // console.log(typeof number);
+// // let newnumber = Boolean(number) 
+// // console.log(typeof newnumber);
 
-// console.log(typeof myName);
-let myNewName = Number (myName)
-console.log(myNewName);
-console.log(typeof myNewName);
+// // console.log(typeof myName);
+// let myNewName = Number (myName)
+// console.log(myNewName);
+// console.log(typeof myNewName);
 
 // "33" => 33
 // "33abc" or "kashif" => NaN
@@ -36,3 +36,37 @@ console.log(typeof myNewName);
 // console.log(typeof valueInNumber);
 
 // console.log(valueInNumber);
+
+//************* Operations *****************/
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let first = "hello"
+let last = " kashif"
+
+let fullName = first + last
+
+// console.log(fullName);
+
+
+
+let a = 7;
+let b = a++;
+
+console.log(a);
+
+let c = 3;
+let d = ++c
+
+console.log(c , d);
